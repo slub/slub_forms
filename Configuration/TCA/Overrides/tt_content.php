a1b5425c91169a2ddd74d8e04f6239574590baac
Alexander Bigga <alexander.bigga@slub-dresden.de>
2018-04-11 10:49:14 +0200

    move registerPlugin() to new location

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,17 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
+}
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'Slub.SlubForms',
+    'Sf',
+    'SLUB: Forms'
+);
+
+$pluginSignature = 'slubforms_sf';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    $pluginSignature,
+    'FILE:EXT:slub_forms/Configuration/FlexForms/flexform_sf.xml'
+);
--- ext_tables.php
+++ ext_tables.php
@@ -3,16 +3,7 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	'Slub.' . $_EXTKEY,
-	'Sf',
-	'SLUB: Forms'
-);
-
-$pluginSignature = str_replace('_','',$_EXTKEY) . '_sf';
-$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_sf.xml');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tt_content.pi_flexform.'.$pluginSignature.'.list', 'EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_be.xlf');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tt_content.pi_flexform.slubforms_sf.list', 'EXT:slub_forms/Resources/Private/Language/locallang_csh_be.xlf');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_slubforms_domain_model_fieldsets', 'EXT:slub_forms/Resources/Private/Language/locallang_csh_tx_slubforms_domain_model_fieldsets.xlf');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_slubforms_domain_model_fieldsets');
