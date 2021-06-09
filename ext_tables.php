2f0663e550fda15eb90ed805bcbbaa3e85cb6638
Alexander Bigga <alexander.bigga@slub-dresden.de>
2021-06-09 11:48:39 +0200

    Bring back csh language files

--- ext_tables.php
+++ ext_tables.php
@@ -1,2 +1,12 @@
 <?php
 defined('TYPO3_MODE') or die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tt_content.pi_flexform.slubforms_sf.list', 'EXT:slub_forms/Resources/Private/Language/locallang_csh_be.xlf');
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_slubforms_domain_model_fieldsets', 'EXT:slub_forms/Resources/Private/Language/locallang_csh_tx_slubforms_domain_model_fieldsets.xlf');
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_slubforms_domain_model_forms', 'EXT:slub_forms/Resources/Private/Language/locallang_csh_tx_slubforms_domain_model_forms.xlf');
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_slubforms_domain_model_fields', 'EXT:slub_forms/Resources/Private/Language/locallang_csh_tx_slubforms_domain_model_fields.xlf');
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_slubforms_domain_model_email', 'EXT:slub_forms/Resources/Private/Language/locallang_csh_tx_slubforms_domain_model_email.xlf');
